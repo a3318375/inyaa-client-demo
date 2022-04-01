d03986218b71926234235b02e790e1be7902a17e
a3318375 <184608371@qq.com>
2022-04-01 22:56:42 +0800

    Initial commit

---
+++ src/main/java/com/inyaa/client/InyaaClientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.inyaa.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InyaaClientDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InyaaClientDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inyaa/client/InyaaClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inyaa.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InyaaClientDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
